ee4bcf7c7baba1164996f482c2c805e38405878f
impossibleno1 <37857357+impossibleno1@users.noreply.github.com>
2018-12-27 17:49:53 +0700

    unit test product

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,147 @@
+﻿using DIENMAYQUYETTIEN2.Areas.Admin.Controllers;
+using DIENMAYQUYETTIEN2.Controllers;
+using DIENMAYQUYETTIEN2.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UnitTestProject1.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var controller = new ProductAdminController();
+            var context = new Mock<HttpContextBase>();
+            var session = new Mock<HttpSessionStateBase>();
+            context.Setup(c => c.Session).Returns(session.Object);
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+            session.Setup(s => s["Username"]).Returns("abc");
+
+            var result = controller.Index() as ViewResult;
+            var db = new DIENMAYQUYETTIENEntities();
+
+
+            //Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(List<Product>));
+            Assert.AreEqual(db.Products.Count(), ((List<Product>)result.Model).Count);
+
+            session.Setup(s => s["Username"]).Returns(null);
+            var redirect = controller.Index() as RedirectToRouteResult;
+            //Assert.AreEqual("Login", redirect.RouteValues["controller"]);
+            Assert.AreEqual("Login", redirect.RouteValues["action"]);
+            
+        }
+
+        [TestMethod]
+        public void TestCreate1()
+        {
+            var controller = new ProductAdminController();
+            var context = new Mock<HttpContextBase>();
+            var session = new Mock<HttpSessionStateBase>();
+            context.Setup(c => c.Session).Returns(session.Object);
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+            session.Setup(s => s["Username"]).Returns("abc");
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.ViewData["ProductTypeID"], typeof(SelectList));
+        }
+
+        [TestMethod]
+        public void TestDetails()
+        {
+            var controller = new ProductAdminController();
+            var context = new Mock<HttpContextBase>();
+            context.Setup(c => c.Server.MapPath("~/App_Data/0")).Returns("~/App_Data/0");
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+
+            var result = controller.Details(0) as FilePathResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("images", result.ContentType);
+            Assert.AreEqual("~/App_Data/0", result.FileName);
+        }
+
+        [TestMethod]
+        public void TestCreate2()
+        {
+            var controller = new ProductAdminController();
+            var context = new Mock<HttpContextBase>();
+            var request = new Mock<HttpRequestBase>();
+            var files = new Mock<HttpFileCollectionBase>();
+            var file = new Mock<HttpPostedFileBase>();
+            context.Setup(c => c.Request).Returns(request.Object);
+            request.Setup(r => r.Files).Returns(files.Object);
+            files.Setup(f => f["Avatar"]).Returns(file.Object);
+            file.Setup(f => f.ContentLength).Returns(1);
+            context.Setup(c => c.Server.MapPath("~/App_Data")).Returns("~/App_Data");
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+
+            var db = new DIENMAYQUYETTIENEntities();
+            var model = new Product();
+            model.ProductTypeID = db.ProductTypes.First().ID;
+            model.ProductName = "TenSP";
+            model.ProductCode = "TVI0001";
+            model.OriginPrice = 123;
+            model.SalePrice = 456;
+            model.InstallmentPrice = 789;
+            model.Quantity = 10;
+
+            using (var scope = new TransactionScope())
+            {
+                var result0 = controller.Create(model) as RedirectToRouteResult;
+                Assert.IsNotNull(result0);
+                file.Verify(f => f.SaveAs(It.Is<string>(s => s.StartsWith("~/App_Data/"))));
+                Assert.AreEqual("Index", result0.RouteValues["action"]);
+
+                file.Setup(f => f.ContentLength).Returns(0);
+                var result1 = controller.Create(model) as ViewResult;
+                Assert.IsNotNull(result1);
+                Assert.IsInstanceOfType(result1.ViewData["ProductTypeID"], typeof(SelectList));
+            }
+        }
+        [TestMethod]
+        public void TestEdit1()
+        {
+            // arrange
+            var _repository = new Mock<IContactRepository>();
+
+            var expectedProduct = new Product
+            {
+                First = "first",
+                Last = "last",
+                Email = "mail@test.com"
+            };
+
+            var mockContext = new Mock<ControllerContext>();
+            _repository.Setup(x => x.GetById(It.IsAny<int>())).Returns(expectedContact);
+
+            var controller = new ContactController(_repository.Object)
+            {
+                ControllerContext = mockContext.Object
+            };
+
+            // act
+            var result = controller.Edit(1) as ViewResult;
+            var resultData = (Contact)result.ViewData.Model;
+
+            // assert
+            Assert.AreEqual("Edit", result.ViewName);
+            Assert.AreEqual(expectedContact.First, resultData.First);
+            Assert.AreEqual(expectedContact.Last, resultData.Last);
+            Assert.AreEqual(expectedContact.Email, resultData.Email);
+        }
+
+    }
+}
